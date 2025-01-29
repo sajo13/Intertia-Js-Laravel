4d10bf72ddd924f750b27952594670a5ad8d9f12
sajo13 <sajosunny007@gmail.com>
2025-01-29 10:20:22 +0530

    Add Inertia Page
    - Inertia Home page is added and data is passed.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ name }}</h1>
+    <p>In this series, we will use the following frameworks:</p>
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+</template>
+
+<script>
+    export  default {
+        props: {
+            name: String,
+            frameworks: Array,
+        }
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home', [
+        'name' => 'Sajo',
+        'frameworks' => [
+            'laravel', 'vue', 'Inertia'
+        ]
+    ]);
 });
