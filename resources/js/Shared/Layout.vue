131a1f584f260f2a915854c55e7e82d8e5587331
sajo13 <sajosunny007@gmail.com>
2025-01-29 14:28:56 +0530

    Add Share data to globally
    - It will be available to every page.

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -2,9 +2,14 @@
 <template>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
-            <h1 class="font-bold text-lg">
-                My App
-            </h1>
+            <div class="flex items-center">
+                <h1 class="font-bold text-lg">
+                    My App
+                </h1>
+                <p class="text-sm ml-4">
+                    Welcome Back, {{ username }}!
+                </p>
+            </div>
 
             <Nav />
         </header>
@@ -21,6 +26,11 @@
 import Nav from "./Nav.vue";
 
 export default {
-    components: { Nav }
+    components: { Nav },
+    computed: {
+        username() {
+            return this.$page.props.auth.user.username;
+        }
+    }
 };
 </script>
